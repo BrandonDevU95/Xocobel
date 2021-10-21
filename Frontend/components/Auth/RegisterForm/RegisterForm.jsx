054789b3b172f7062a324f651152c17e7259fd4f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-20 21:03:14 -0500

    Diseño formulario registro

--- Frontend/components/Auth/RegisterForm/RegisterForm.jsx
+++ Frontend/components/Auth/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
 
 export default function RegisterForm({ showLoginForm }) {
    return (
-      <div>
-         <h1>Estamos en register form</h1>
-         <button onClick={showLoginForm}>Login</button>
-      </div>
+      <Form className="login-form">
+         <Form.Input name="name" type="text" placeholder="Nombre" />
+         <Form.Input name="lastname" type="text" placeholder="Apellidos" />
+         <Form.Input name="username" type="text" placeholder="Usuario" />
+         <Form.Input name="email" type="text" placeholder="Correo" />
+         <Form.Input name="password" type="password" placeholder="Contraseña" />
+         <div className="actions">
+            <Button type="button" basic>
+               Iniciar Sesion
+            </Button>
+            <Button type="submit" className="submit">
+               Registrar
+            </Button>
+         </div>
+      </Form>
    );
 }
