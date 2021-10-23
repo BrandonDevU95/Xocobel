318af4bbaf96d0346810ce039b6ae9f53fdc713c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-23 12:03:24 -0500

    Bloqueo de pagina account y estilos

--- Frontend/pages/account.js
+++ Frontend/pages/account.js
@@ -1,9 +1,39 @@
+import { getMeApi } from '../api/user';
+import useAuth from '../hooks/useAuth';
+import { useRouter } from 'next/router';
 import BasicLayout from '../layouts/Basic';
+import { useState, useEffect } from 'react';
 
 export default function account() {
+   const router = useRouter();
+   const { auth, logout } = useAuth();
+   const [user, setUser] = useState(undefined);
+
+   useEffect(() => {
+      (async () => {
+         const response = await getMeApi(logout);
+         setUser(response || null);
+      })();
+   }, [auth]);
+
+   if (user === undefined) return null;
+   if (!auth && !user) {
+      router.replace('/');
+      return null;
+   }
+
    return (
       <BasicLayout className="account">
-         <h1>Account</h1>
+         <Configuration />
       </BasicLayout>
+   );
+}
+
+function Configuration() {
+   return (
+      <div className="account__configuration">
+         <div className="title">Configuracion</div>
+         <div className="data">Formularios</div>
+      </div>
    );
 }
