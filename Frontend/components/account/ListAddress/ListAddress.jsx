56faa7b3d7e946d976b3568f02175a3e6a022344
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-23 22:39:00 -0500

    Listando direcciones

--- Frontend/components/account/ListAddress/ListAddress.jsx
+++ Frontend/components/account/ListAddress/ListAddress.jsx
@@ -1,6 +1,8 @@
+import { map, size } from 'lodash';
 import { useState, useEffect } from 'react';
-import { getAddressApi } from '../../../api/address';
 import useAuth from '../../../hooks/useAuth';
+import { Grid, Button } from 'semantic-ui-react';
+import { getAddressApi } from '../../../api/address';
 
 export default function ListAddress() {
    const { auth, logout } = useAuth();
@@ -14,8 +16,41 @@ export default function ListAddress() {
    }, []);
 
    return (
-      <div>
-         <h1>List Address</h1>
+      <div className="list-address">
+         {size(addresses) === 0 ? (
+            <h3>No hay direcciones</h3>
+         ) : (
+            <Grid>
+               {map(addresses, (address) => (
+                  <Grid.Column
+                     key={address.id}
+                     mobile={16}
+                     tablet={8}
+                     computer={4}
+                  >
+                     <Address address={address} />
+                  </Grid.Column>
+               ))}
+            </Grid>
+         )}
+      </div>
+   );
+}
+
+function Address({ address }) {
+   return (
+      <div className="address">
+         <p>{address.title}</p>
+         <p>{address.name}</p>
+         <p>{address.address}</p>
+         <p>
+            {address.state}, {address.city} {address.postalCode}
+         </p>
+         <p>{address.phone}</p>
+         <div className="actions">
+            <Button primary>Editar</Button>
+            <Button>Eliminar</Button>
+         </div>
       </div>
    );
 }
