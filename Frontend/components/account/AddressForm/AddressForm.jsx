728180225ca9b73b811f8095cc2a9aec0146e7bc
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-23 21:36:54 -0500

    Gestionando y validando direcciones del formulario

--- Frontend/components/account/AddressForm/AddressForm.jsx
+++ Frontend/components/account/AddressForm/AddressForm.jsx
@@ -1,13 +1,29 @@
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { useFormik } from 'formik';
 import { Form, Button } from 'semantic-ui-react';
 
 export default function AddressForm() {
+   const [loading, setLoading] = useState(false);
+
+   const formik = useFormik({
+      initialValues: initialValues(),
+      validationSchema: Yup.object(validationSchema()),
+      onSubmit: (formData) => {
+         console.log(formData);
+      },
+   });
+
    return (
-      <Form className="address-form">
+      <Form className="address-form" onSubmit={formik.handleSubmit}>
          <Form.Input
             name="title"
             type="text"
             label="Titulo de la direccion"
             placeholder="Titulo de la direccion"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+            error={formik.errors.title}
          />
          <Form.Group widths="equal">
             <Form.Input
@@ -15,12 +31,18 @@ export default function AddressForm() {
                type="text"
                label="Nombre completo"
                placeholder="Nombre completo"
+               onChange={formik.handleChange}
+               value={formik.values.name}
+               error={formik.errors.name}
             />
             <Form.Input
                name="address"
                type="text"
                label="Direccion"
                placeholder="Direccion"
+               onChange={formik.handleChange}
+               value={formik.values.address}
+               error={formik.errors.address}
             />
          </Form.Group>
          <Form.Group widths="equal">
@@ -29,12 +51,18 @@ export default function AddressForm() {
                type="text"
                label="Ciudad / Municipio"
                placeholder="Ciudad"
+               onChange={formik.handleChange}
+               value={formik.values.city}
+               error={formik.errors.city}
             />
             <Form.Input
                name="state"
                type="text"
                label="Estado"
                placeholder="Estado"
+               onChange={formik.handleChange}
+               value={formik.values.state}
+               error={formik.errors.state}
             />
          </Form.Group>
          <Form.Group widths="equal">
@@ -43,19 +71,49 @@ export default function AddressForm() {
                type="text"
                label="Codigo Postal"
                placeholder="Codigo Postal"
+               onChange={formik.handleChange}
+               value={formik.values.postalCode}
+               error={formik.errors.postalCode}
             />
             <Form.Input
                name="phone"
                type="text"
                label="Telefono"
                placeholder="Telefono"
+               onChange={formik.handleChange}
+               value={formik.values.phone}
+               error={formik.errors.phone}
             />
          </Form.Group>
          <div className="actions">
-            <Button type="submit" className="submit">
+            <Button type="submit" className="submit" loading={loading}>
                Crear direccion
             </Button>
          </div>
       </Form>
    );
+}
+
+function initialValues() {
+   return {
+      title: '',
+      name: '',
+      address: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      phone: '',
+   };
+}
+
+function validationSchema() {
+   return {
+      title: Yup.string().required(true),
+      name: Yup.string().required(true),
+      address: Yup.string().required(true),
+      city: Yup.string().required(true),
+      state: Yup.string().required(true),
+      postalCode: Yup.string().required(true),
+      phone: Yup.string().required(true),
+   };
 }
