d355a1c960dec5a33c7aa25ab44728a3949b1b7b
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-26 19:36:46 -0500

    Estructura y estilos de la lista de productos

--- Frontend/components/ListProducts/ListProducts.jsx
+++ Frontend/components/ListProducts/ListProducts.jsx
@@ -1,11 +1,44 @@
 import { map } from 'lodash';
+import Link from 'next/link';
+import { Grid, Image } from 'semantic-ui-react';
+import { BASE_PATH } from '../../utils/constants';
 
 export default function ListProducts({ products }) {
    return (
-      <div className="list-games">
-         {map(products, (product) => (
-            <h3>{product.title}</h3>
-         ))}
+      <div className="list-products">
+         <Grid>
+            <Grid.Row columns={5}>
+               {map(products, (product) => (
+                  <Product key={product._id} product={product} />
+               ))}
+            </Grid.Row>
+         </Grid>
       </div>
+   );
+}
+
+function Product({ product }) {
+   return (
+      <Grid.Column className="list-products__product">
+         <Link href={`/boutique/${product.url}`}>
+            <a>
+               <div className="list-products__product-poster">
+                  <Image
+                     src={`${BASE_PATH}${product.poster.url}`}
+                     alt={product.title}
+                  />
+                  <div className="list-products__product-poster-info">
+                     {product.discount ? (
+                        <span className="discount">-{product.discount}%</span>
+                     ) : (
+                        <span />
+                     )}
+                     <span className="price">${product.price}</span>
+                  </div>
+               </div>
+               <h2>{product.title}</h2>
+            </a>
+         </Link>
+      </Grid.Column>
    );
 }
