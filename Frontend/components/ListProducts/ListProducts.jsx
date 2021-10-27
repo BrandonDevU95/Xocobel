24dbcd86080d1ac33e01a43509d570a079545c98
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-26 19:48:27 -0500

    Lista de productos responsive

--- Frontend/components/ListProducts/ListProducts.jsx
+++ Frontend/components/ListProducts/ListProducts.jsx
@@ -2,12 +2,33 @@ import { map } from 'lodash';
 import Link from 'next/link';
 import { Grid, Image } from 'semantic-ui-react';
 import { BASE_PATH } from '../../utils/constants';
+import {
+   breakpointUpSm,
+   breakpointUpMd,
+   breakpointUpLg,
+} from '../../utils/breakpoint';
+import useWindowSize from '../../hooks/useWindowSize';
 
 export default function ListProducts({ products }) {
+   const { width } = useWindowSize();
+
+   const getColumnsRender = () => {
+      switch (true) {
+         case width > breakpointUpLg:
+            return 5;
+         case width > breakpointUpMd:
+            return 3;
+         case width > breakpointUpSm:
+            return 2;
+         default:
+            return 1;
+      }
+   };
+
    return (
       <div className="list-products">
          <Grid>
-            <Grid.Row columns={5}>
+            <Grid.Row columns={getColumnsRender()}>
                {map(products, (product) => (
                   <Product key={product._id} product={product} />
                ))}
---
+++ Frontend/utils/breakpoint.js
@@ -0,0 +1,8 @@
+export const breakpointUpSm = 576;
+export const breakpointUpMd = 768;
+export const breakpointUpLg = 992;
+export const breakpointUpXl = 1200;
+export const breakpointDownSm = 575;
+export const breakpointDownMd = 767;
+export const breakpointDownLg = 991;
+export const breakpointDownXl = 1199;
