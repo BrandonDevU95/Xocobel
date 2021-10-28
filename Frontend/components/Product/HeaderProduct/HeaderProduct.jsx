df8ea5585a424946abf9b0d0f01d84e437641d64
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-28 16:31:17 -0500

    Añadiendo informacion del juego

--- Frontend/components/Product/HeaderProduct/HeaderProduct.jsx
+++ Frontend/components/Product/HeaderProduct/HeaderProduct.jsx
@@ -14,8 +14,38 @@ export default function HeaderProduct({ product }) {
             />
          </Grid.Column>
          <Grid.Column mobile={16} tablet={10} computer={11}>
-            <p>Game info</p>
+            <Info product={product} />
          </Grid.Column>
       </Grid>
+   );
+}
+
+function Info({ product }) {
+   return (
+      <>
+         <div className="header-product__title">
+            {product.title}
+            <Icon name="heart" className="like" link />
+         </div>
+         <div className="header-product__delivery">Entrega en 24/48 horas</div>
+         <div
+            className="header-product__description"
+            dangerouslySetInnerHTML={{ __html: product.description }}
+         />
+         <div className="header-product__buy">
+            <div className="header-product__buy-price">
+               <p>Precio de venta: ${product.price}</p>
+               <div className="header-product__buy-price-actions">
+                  <p>-{product.discount}%</p>
+                  <p>
+                     $
+                     {product.price -
+                        Math.floor(product.price * product.discount) / 100}
+                  </p>
+               </div>
+            </div>
+            <Button className="header-product__buy-button">Comprar</Button>
+         </div>
+      </>
    );
 }
