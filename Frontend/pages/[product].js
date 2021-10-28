164e114e28b53d44efef2f27de3c5c665db810e0
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-28 15:39:05 -0500

    Obteniendo los datos del producto por url

--- Frontend/pages/[product].js
+++ Frontend/pages/[product].js
@@ -1,10 +1,19 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import { getProductByUrlApi } from '../api/products';
 import BasicLayout from '../layouts/Basic/BasicLayout';
 
 export default function Product() {
    const { query } = useRouter();
-   console.log(query.product);
+   const [product, setProduct] = useState(null);
+
+   useEffect(() => {
+      (async () => {
+         const response = await getProductByUrlApi(query.product);
+         setProduct(response);
+      })();
+   }, [query]);
+
    return (
       <BasicLayout className="product">
          <h1>Estamos en product</h1>
