75aeb5f302fbc1b1ac46b9826007a3832cd56034
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-28 16:55:29 -0500

    Agregando componentes de ingredientes y nutricion

--- Frontend/components/Product/TabsProduct/TabsProduct.jsx
+++ Frontend/components/Product/TabsProduct/TabsProduct.jsx
@@ -5,13 +5,40 @@ export default function TabsProduct({ product }) {
    const panes = [
       {
          menuItem: 'Ingredients',
-         render: () => <Tab.Pane>Info ingredients</Tab.Pane>,
+         render: () => (
+            <Tab.Pane>
+               <IngredientsTab product={product} />{' '}
+            </Tab.Pane>
+         ),
       },
       {
          menuItem: 'Nutrition Values',
-         render: () => <Tab.Pane>Info nutrients</Tab.Pane>,
+         render: () => (
+            <Tab.Pane>
+               {' '}
+               <NutritionValuesTab product={product} />{' '}
+            </Tab.Pane>
+         ),
       },
    ];
 
    return <Tab className="tabs-product" panes={panes} />;
+}
+
+function IngredientsTab({ product }) {
+   return (
+      <div className="tabs-product__ingredients">
+         <h2>{product.title}</h2>
+         <h3>{product.ingredients}</h3>
+      </div>
+   );
+}
+
+function NutritionValuesTab({ product }) {
+   return (
+      <div className="tabs-product__nutrition">
+         <h2>{product.title}</h2>
+         <h3>{product.nutrition}</h3>
+      </div>
+   );
 }
