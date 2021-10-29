42735c22d4644dcc008a3ee1d36e6d29d1b3a009
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-29 18:07:35 -0500

    Abriendo las imagenes de galeria al hacer click

--- Frontend/components/Product/CarouselScreen/CarouselScreen.jsx
+++ Frontend/components/Product/CarouselScreen/CarouselScreen.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { map, size } from 'lodash';
+import { useState } from 'react';
 import Slider from 'react-slick';
-import { Image } from 'semantic-ui-react';
+import { map, size } from 'lodash';
+import { Image, Modal } from 'semantic-ui-react';
 import { BASE_PATH } from '../../../utils/constants';
 
 export default function CarouselScreen({ title, screenShots }) {
+   const [showModal, setShowModal] = useState(false);
+   const [urlImage, setUrlImage] = useState(null);
+
+   const openImage = (url) => {
+      setUrlImage(url);
+      setShowModal(true);
+   };
+
    const settings = {
       className: 'carousel-screen',
       dots: false,
@@ -14,15 +22,24 @@ export default function CarouselScreen({ title, screenShots }) {
       swipeToSlide: true,
    };
    return (
-      <Slider {...settings}>
-         {map(screenShots, (screenShot) => (
-            <Image
-               key={screenShot.id}
-               src={`${BASE_PATH}${screenShot.url}`}
-               alt={screenShot.name}
-               onClick={() => console.log('Abrir imagen...')}
-            />
-         ))}
-      </Slider>
+      <>
+         <Slider {...settings}>
+            {map(screenShots, (screenShot) => (
+               <Image
+                  key={screenShot.id}
+                  src={`${BASE_PATH}${screenShot.url}`}
+                  alt={screenShot.name}
+                  onClick={() => openImage(`${BASE_PATH}${screenShot.url}`)}
+               />
+            ))}
+         </Slider>
+         <Modal
+            open={showModal}
+            onClose={() => setShowModal(false)}
+            size="small"
+         >
+            <Image src={urlImage} alt={title} />
+         </Modal>
+      </>
    );
 }
