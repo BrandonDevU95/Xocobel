e25d8931bce74ce32de387cb5b66c679ce37dc70
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-29 19:23:48 -0500

    Obteniendo todos los juegos añadidos a la lista

--- Frontend/pages/wishlist.js
+++ Frontend/pages/wishlist.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { size, forEach } from 'lodash';
+import { useState, useEffect } from 'react';
 import BasicLayout from '../layouts/Basic';
+import useAuth from '../hooks/useAuth';
+import { getFavoriteApi } from '../api/favorite';
 
 export default function wishlist() {
+   const { auth, logout } = useAuth();
+   const [product, setProduct] = useState(null);
+
+   useEffect(() => {
+      (async () => {
+         const response = await getFavoriteApi(auth.idUser, logout);
+         setProduct(response);
+      })();
+   }, []);
+
    return (
       <BasicLayout className="wishlist">
          <div className="wishlist__block">
