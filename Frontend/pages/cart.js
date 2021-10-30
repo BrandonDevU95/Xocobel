9a2f6f7ee025cb1f38888ed4a5b7ab642e5a21f8
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-29 23:12:00 -0500

    Creando la pagina del carrito

---
+++ Frontend/pages/cart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicLayout from '../layouts/Basic';
+
+export default function cart() {
+   return (
+      <BasicLayout className="cart">
+         <h1>Carrito</h1>
+      </BasicLayout>
+   );
+}
