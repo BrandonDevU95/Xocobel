dab6c204634e7771a8da0a73dc992dd44e1342c7
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-29 21:17:30 -0500

    Obteniendo resultados de la busqueda

--- Frontend/api/products.js
+++ Frontend/api/products.js
@@ -53,3 +53,15 @@ export async function getProductByUrlApi(path) {
       return null;
    }
 }
+
+export async function searcgProductsApi(search) {
+   try {
+      const url = `${BASE_PATH}/products?_q=${search}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      return result;
+   } catch (error) {
+      console.log(error);
+      return null;
+   }
+}
--- Frontend/pages/search.js
+++ Frontend/pages/search.js
@@ -1,10 +1,28 @@
-import { useState, useEffect } from 'react';
+import { size } from 'lodash';
+import { useRouter } from 'next/router';
 import BasicLayout from '../layouts/Basic';
+import { useState, useEffect } from 'react';
+import { searcgProductsApi } from '../api/products';
 
 export default function search() {
+   const [products, setProducts] = useState(null);
+   const { query } = useRouter();
+
    useEffect(() => {
       document.getElementById('xoco-search').focus();
    }, []);
+
+   useEffect(() => {
+      (async () => {
+         if (size(query.query) > 0) {
+            const response = await searcgProductsApi(query.query);
+            if (size(response) > 0) setProducts(response);
+            else setProducts([]);
+         } else {
+            setProducts([]);
+         }
+      })();
+   }, [query]);
 
    return (
       <BasicLayout className="search">
