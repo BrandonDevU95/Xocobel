114961682c6f5267007dd4bdef8ecba008abcee2
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-30 17:19:34 -0500

    Obteniendo los pedidos del usuario

--- Frontend/pages/orders.js
+++ Frontend/pages/orders.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import useAuth from '../hooks/useAuth';
+import { getOrdersApi } from '../api/order';
+
 import BasicLayout from '../layouts/Basic';
 
 export default function Orders() {
+   const { auth, logout } = useAuth();
+   const [order, setOrder] = useState(null);
+
+   useEffect(() => {
+      (async () => {
+         const response = await getOrdersApi(auth.idUser);
+         setOrder(response || []);
+      })();
+   }, []);
+
    return (
       <BasicLayout className="orders">
          <div className="orders__block">
