20a01f1f58303810f028b21714d28b297297e23f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-30 01:01:39 -0500

    Mostrando lista de direcciones de envio

--- Frontend/components/Cart/ShippingAddress/ShippingAddress.jsx
+++ Frontend/components/Cart/ShippingAddress/ShippingAddress.jsx
@@ -18,8 +18,45 @@ export default function ShippingAddress() {
    }, []);
 
    return (
-      <div>
-         <h1>ShippingAddress</h1>
+      <div className="shipping-address">
+         <div className="title">Direccion de envio</div>
+         <div className="data">
+            {size(addresses) === 0 ? (
+               <h3>
+                  No hay direcciones registradas,{' '}
+                  <Link href="/address">
+                     <a>Agregar direccion</a>
+                  </Link>
+               </h3>
+            ) : (
+               <Grid>
+                  {map(addresses, (address) => (
+                     <Grid.Column
+                        key={address.id}
+                        mobile={16}
+                        tablet={8}
+                        computer={4}
+                     >
+                        <Address address={address} />
+                     </Grid.Column>
+                  ))}
+               </Grid>
+            )}
+         </div>
+      </div>
+   );
+}
+
+function Address({ address }) {
+   return (
+      <div className="address">
+         <p>{address.title}</p>
+         <p>{address.name}</p>
+         <p>{address.address}</p>
+         <p>
+            {address.state}, {address.city} {address.postalCode}
+         </p>
+         <p>{address.phone}</p>
       </div>
    );
 }
