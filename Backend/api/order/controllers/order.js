4b1f8acba8cc437fc85b089bd03720360c569417
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-30 15:36:04 -0500

    Funcion del back para realizar el pedido y pago

--- Backend/api/order/controllers/order.js
+++ Backend/api/order/controllers/order.js
@@ -1,8 +1,44 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51JqMw4FDgkDMxEY6LQ4BPyzxLncEItpaI6GEQO6zhtZtUW9rck8s30rpkEEvUJcWNIlc1QnDPlakcRzdF3QWDPY800awR2ApYB"
+);
+// TODO: Exportar clave provada a .env
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body;
+    let totalPayment = 0;
+    products.forEach((product) => {
+      totalPayment += product.price;
+    });
+    const charge = await stripe.charge.create({
+      amount: totalPayment * 100,
+      currency: "mxn",
+      description: `ID Usuario: ${idUser}`,
+      source: token.id,
+    });
+
+    const createOrder = [];
+    for await (const product of products) {
+      const data = {
+        product: product.id,
+        user: idUser,
+        totalPayment,
+        idPayment: charge.id,
+        addressShipping,
+      };
+      const validData = await strapi.entityValidator.validateEntity(
+        strapi.model.order,
+        data
+      );
+      const entry = await strapi.query("order").create(validData);
+      createOrder.push(entry);
+    }
+    return createOrder;
+  },
+};
