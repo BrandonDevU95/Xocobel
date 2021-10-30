2a4b010b8196651a50255c5a8e7ac11b0f542dea
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-29 20:48:36 -0500

    Renderizando lista de favoritos

--- Frontend/pages/wishlist.js
+++ Frontend/pages/wishlist.js
@@ -1,17 +1,28 @@
 import { size, forEach } from 'lodash';
-import { useState, useEffect } from 'react';
-import BasicLayout from '../layouts/Basic';
 import useAuth from '../hooks/useAuth';
+import BasicLayout from '../layouts/Basic';
+import { useState, useEffect } from 'react';
 import { getFavoriteApi } from '../api/favorite';
+import ListProducts from '../components/ListProducts';
+import { Loader } from 'semantic-ui-react';
 
 export default function wishlist() {
    const { auth, logout } = useAuth();
-   const [product, setProduct] = useState(null);
+   const [products, setProducts] = useState(null);
 
    useEffect(() => {
       (async () => {
          const response = await getFavoriteApi(auth.idUser, logout);
-         setProduct(response);
+         if (size(response) > 0) {
+            const productsList = [];
+            forEach(response, (data) => {
+               console.log(data);
+               productsList.push(data.product);
+            });
+            setProducts(productsList);
+         } else {
+            setProducts([]);
+         }
       })();
    }, []);
 
@@ -20,7 +31,21 @@ export default function wishlist() {
          <div className="wishlist__block">
             <div className="title">Lista de deseos</div>
             <div className="data">
-               <p>Lista de productos</p>
+               {!products && <Loader active>Cargando Productos</Loader>}
+               {products && size(products) === 0 && (
+                  <div className="data__not-found">
+                     <h3>No hay productos</h3>
+                  </div>
+               )}
+               {size(products) > 0 && <ListProducts products={products} />}
+               {/* TODO: Agregar la paginacion a favoritos */}
+               {/* {size(products) > 0 && totalProducts ? (
+                  <Pagination
+                     totalProducts={totalProducts}
+                     page={query.page ? parseInt(query.page) : 1}
+                     limitPerPage={limitPerPage}
+                  />
+               ) : null} */}
             </div>
          </div>
       </BasicLayout>
