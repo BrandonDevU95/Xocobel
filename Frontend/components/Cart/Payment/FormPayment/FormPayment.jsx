62acd293ae66e9dc24db1c18e560b4246603d0ac
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-30 15:17:35 -0500

    Creando acuerdo de cobro con Stripe

--- Frontend/components/Cart/Payment/FormPayment/FormPayment.jsx
+++ Frontend/components/Cart/Payment/FormPayment/FormPayment.jsx
@@ -8,14 +8,32 @@ import useCart from '../../../../hooks/useCart';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 export default function FormPayment({ products, address }) {
-   const handleSubmit = (event) => {
+   const stripe = useStripe();
+   const elements = useElements();
+   const [loading, setLoading] = useState(false);
+
+   const handleSubmit = async (event) => {
       event.preventDefault();
+      setLoading(true);
+      if (!stripe && !elements) return null;
+
+      const cardElement = elements.getElement(CardElement);
+      const result = await stripe.createToken(cardElement);
+
+      if (result.error) {
+         toast.error(result.error.message);
+      } else {
+         console.log(result);
+      }
+      setLoading(false);
    };
 
    return (
       <form className="form-payment" onSubmit={handleSubmit}>
          <CardElement />
-         <Button type="submit">Pagar</Button>
+         <Button type="submit" loading={loading} disabled={!stripe}>
+            Pagar
+         </Button>
       </form>
    );
 }
