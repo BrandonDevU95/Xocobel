4c10adbc94de0bd44d0189a7ebc7703a90dc205f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-30 19:20:30 -0500

    Terminando de dar estilo a los pedidos

--- Frontend/components/Orders/Order/Order.jsx
+++ Frontend/components/Orders/Order/Order.jsx
@@ -9,7 +9,7 @@ import 'moment/locale/es-mx';
 export default function Order({
    order: {
       product: { title, poster, url },
-      totalPaymet,
+      totalPayment,
       createdAt,
       addressShiping,
    },
@@ -18,11 +18,29 @@ export default function Order({
       <>
          <div className="order">
             <div className="order__info">
-               <Link href={`/${url}`}>
-                  <a>
-                     <Image src={`${BASE_PATH}${poster.url}`} alt={title} />
-                  </a>
-               </Link>
+               <div className="order__info-data">
+                  <Link href={`/${url}`}>
+                     <a>
+                        <Image src={`${BASE_PATH}${poster.url}`} alt={title} />
+                     </a>
+                  </Link>
+                  <div>
+                     <h2>{title}</h2>
+                     <p>${totalPayment}</p>
+                  </div>
+               </div>
+               <div className="order__info-other">
+                  <p className="order__info-other-date">
+                     {moment(createdAt).format('L')} -{' '}
+                     {moment(createdAt).format('LT')}
+                  </p>
+                  <Icon
+                     name="eye"
+                     circular
+                     link
+                     onClick={() => console.log('Info')}
+                  />
+               </div>
             </div>
          </div>
       </>
