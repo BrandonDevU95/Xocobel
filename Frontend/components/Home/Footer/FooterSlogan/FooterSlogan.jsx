1bd9f6de4ce580bb8940db5ea91d59e5b34a3614
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-01 20:25:15 -0600

    Añadiendo Slogan y Corona en footer

--- Frontend/components/Home/Footer/FooterSlogan/FooterSlogan.jsx
+++ Frontend/components/Home/Footer/FooterSlogan/FooterSlogan.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Container, Image } from 'semantic-ui-react';
 
 export default function FooterSlogan() {
    return (
-      <div>
-         <h1>Footer Slogan</h1>
+      <div className="footer-slogan">
+         <Container fluid className="footer-slogan-container">
+            <p style={{ paddingRight: '1.5rem' }}>Todo el sabor europeo</p>
+            <Image src="/corona.png" />
+            <p style={{ paddingLeft: '1.5rem' }}>Desde 1987</p>
+         </Container>
       </div>
    );
 }
