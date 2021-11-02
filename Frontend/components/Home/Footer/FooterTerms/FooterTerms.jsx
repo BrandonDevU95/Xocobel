5d9f38f1d2465d7b5e75c5867144be2e5ba94821
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-01 22:00:31 -0600

    Añadiendo terminos y condiciones a Footer

--- Frontend/components/Home/Footer/FooterTerms/FooterTerms.jsx
+++ Frontend/components/Home/Footer/FooterTerms/FooterTerms.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container, Icon } from 'semantic-ui-react';
 
 export default function FooterTerms() {
    return (
-      <div>
-         <h1>FooterTerms</h1>
+      <div className="footer-terms">
+         <Container fluid className="footer-terms-container">
+            <div className="footer-terms-container_content">
+               <div className="footer-terms-container_content_left">
+                  <Icon name="copyright outline" />
+                  <p>Xocobel SA DE CV</p>
+               </div>
+               <div className="footer-terms-container_content_right">
+                  <p style={{ paddingRight: '1rem' }}>Privacy & Cookies</p>
+                  <p>Terms & Conditions</p>
+               </div>
+            </div>
+         </Container>
       </div>
    );
 }
