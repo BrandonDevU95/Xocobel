6f7b40783442ee350df4e72b87c8b8265de3e3ca
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-06 17:16:08 -0600

    Creando pagina de xocobel

---
+++ Frontend/pages/xocobel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import BasicLayout from '../layouts/Basic';
+
+export default function xocobel() {
+   return (
+      <BasicLayout className="xocobel">
+         <Container fluid className="xocobel-container">
+            <h1>Xocobel</h1>
+         </Container>
+      </BasicLayout>
+   );
+}
