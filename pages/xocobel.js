fb245deaf408043660baa6cc9e3bd4e7001dca03
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-27 16:35:10 -0600

    Change info to Xocobel

--- pages/xocobel.js
+++ pages/xocobel.js
@@ -11,20 +11,31 @@ export default function xocobel() {
                <div className="xocobel-container-banner-info">
                   <h2>XOCOBEL</h2>
                   <p>
-                     From its origins as a pharmacist to its present calling as
-                     a <br />
-                     chocolatier, Neuhaus has always put caring for others and
-                     ensuring <br />
-                     their happiness first. That has become the core thread of
-                     our DNA. <br />
-                     And we extend this consideration for others via our
-                     commitment to <br />
-                     honest chocolate.
+                     Somos una empresa con más de 30 años de experiencia en el
+                     <br />
+                     mercado. Nos dedicamos a la producción de chocolatería y
+                     <br />
+                     bombonería artesanal elaborada por expertos chocolateros
+                     mexicanos.
                   </p>
                </div>
             </div>
             <div className="xocobel-container-band">
-               <p>OUR COMMITMENT TO HONEST CHOCOLATE</p>
+               <h3>NUESTRA MISION</h3>
+               <p>
+                  Nuestra misión se basa en contribuir al desarrollo de la
+                  <br />
+                  industria nacional chocolatera a través de la producción
+                  <br />
+                  eficiente y responsable de chocolatería fina de la mas alta
+                  <br />
+                  calidad, permitiendonos llegar a diferentes categorías y<br />
+                  nuevos mercados, promoviendo la reinvención e innovación
+                  <br />
+                  continua, teniendo como principal objetivo ser una compañía
+                  <br />
+                  líder en la industria chocolatera a nivel internacional.
+               </p>
             </div>
             <div className="xocobel-container-grid-1">
                <Grid>
@@ -38,15 +49,11 @@ export default function xocobel() {
                      <div className="xocobel-container-grid-1_column-description">
                         <h3>SUSTAINABLY SOURCED CACAO</h3>
                         <p>
-                           UTZ (now part of the Rainforest Alliance) is the
-                           world's leading programme for sustainably grown
-                           cacao. The UTZ label stands for more sustainable
-                           farming and better opportunities for farmers, their
-                           families, and our planet. The UTZ certification
-                           program enables farmers to use better farming
-                           methods, grow better crops, and generate more income.
-                           They learn how to improve working conditions, adapt
-                           to climate change, and protect the environment.
+                           Nuestro objetivo es poder permanecer en el gusto del
+                           mercado a través de la innovación continua, adoptando
+                           nuevas tendencias culinarias que nos permitan
+                           permanecer posicionados como una de las marcas
+                           premium en el mercado.
                         </p>
                      </div>
                   </Grid.Column>
@@ -61,17 +68,10 @@ export default function xocobel() {
                      <div className="xocobel-container-grid-2_column-description">
                         <h3>WE INVEST IN OUR OWN CACAO FARM</h3>
                         <p>
-                           Our own plantation is located in in the Guayaquil
-                           region of Ecuador in South America. This location
-                           offers the ideal conditions for growing cacao trees.
-                           The combination of the favourable climate, the
-                           perfect soil and the expertise of our local farmers
-                           guarantees the superior quality of our chocolate. The
-                           cacao farmers are true masters of the fermentation
-                           process, which releases the characteristic aromas of
-                           the cacao beans to perfection so that we can enjoy
-                           chocolate with a unique, distinctive and rich
-                           flavour.
+                           Algunos de nuestros productos son: Chocolatines,
+                           Trufas, Mazapanes, Ganaches, Galletas, Pralinés,
+                           Grajeados, Jarabes, Barras Nutrimentales, Pasta de
+                           frutas, entre otros.
                         </p>
                      </div>
                   </Grid.Column>
@@ -92,9 +92,10 @@ export default function xocobel() {
                      <div className="xocobel-container-grid-1_column-description">
                         <h3>FROM BEAN TO PRALINE</h3>
                         <p>
-                           The beans of the cacao tree have a naturally bitter
-                           taste and must first pass through several stages in
-                           order to develop the perfect flavour.
+                           Equipo de última generación, con una capacidad de
+                           producción en toneladas diarias. Personal altamente
+                           calificado con los más altos estándares de calidad,
+                           seguridad e higiene.
                         </p>
                      </div>
                   </Grid.Column>
@@ -109,12 +110,9 @@ export default function xocobel() {
                      <div className="xocobel-container-grid-2_column-description">
                         <h3>NATURAL INGREDIENTS</h3>
                         <p>
-                           Our buyers carefully select all the ingredients,
-                           flavourings and colourings that go into a Neuhaus
-                           praline. They choose the best farmers and craftsmen
-                           from around the world to ensure that every Neuhaus
-                           product is, without exception, 100% natural and
-                           guarantees a symphony of flavours and textures.
+                           Certificación ante Global Foof Safety Initiative.
+                           Líneas de producción altamente eficientes y
+                           flexibles.
                         </p>
                      </div>
                   </Grid.Column>
@@ -125,18 +123,19 @@ export default function xocobel() {
             </div>
             <div className="xocobel-container-band-foot">
                <div className="xocobel-container-band-foot-data">
-                  <h3>OUR COMMITMENT FOR THE FUTURE</h3>
+                  <h3>PROPOSITO</h3>
                   <p>
-                     When selecting our ingredients and packaging, we always
-                     make conscious choices. <br />
-                     We strive to use these choices to contribute to a
-                     sustainable future. <br /> This objective is of great
-                     importance to us and it is an ongoing process, which never
-                     ends.
+                     El éxito de Xocobel se ha basado en un crecimiento
+                     <br />
+                     sostenido año con año, mediante la oferta de productos de
+                     <br />
+                     únicos e innovadores. Todos los días, hacemos un esfuerzo
+                     <br />
+                     adicional para liderar el futuro de la <br /> chocolateria
+                     fina en México y el mundo.
                   </p>
                </div>
             </div>
-            <Shipping />
          </Container>
       </BasicLayout>
    );
