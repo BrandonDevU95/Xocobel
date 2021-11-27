cc53baa86369ba1a5541155eff9707c9a3994dbf
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-26 20:04:22 -0600

    Change Iconic Products titles

--- components/Home/IconicProducts/IconicProducts.jsx
+++ components/Home/IconicProducts/IconicProducts.jsx
@@ -2,54 +2,51 @@ import { Button, Container, Grid, Image } from 'semantic-ui-react';
 
 export default function IconicProducts() {
    return (
-      <Container fluid className="iconic-products__grid">
-         <Grid>
-            <Grid.Column width={8}>
-               <div className="iconic-products__grid-content">
-                  <Image src="/Popular1.jpeg" fluid alt="Product 1" />
-                  <div className="description">
-                     <p>Semi Esfera</p>
-                     <p>de Chocolate</p>
+      <section className="iconic-products">
+         <Container fluid className="iconic-products_container">
+            <Grid>
+               <Grid.Column width={8}>
+                  <div className="iconic-products_container-content">
+                     <Image src="/Popular1.jpeg" fluid alt="Product 1" />
+                     <div className="iconic-products_container-content-description">
+                        <p>Chocolate</p>
+                        <p>Con leche</p>
+                     </div>
+                     <div className="iconic-products_container-content-btnAction-right_1">
+                        <Button type="button" primary>
+                           MÁS
+                        </Button>
+                     </div>
                   </div>
-                  <div className="btnAction-right_1">
-                     <Button type="button" primary>
-                        MORE
-                     </Button>
+                  <div className="iconic-products_container-content">
+                     <Image src="/Popular2.jpeg" fluid alt="Product 2" />
+                     <div className="iconic-products_container-content-description">
+                        <p>Chocolate</p>
+                        <p>Blanco</p>
+                     </div>
+                     <div className="iconic-products_container-content-btnAction-left">
+                        <Button type="button" primary>
+                           MÁS
+                        </Button>
+                     </div>
                   </div>
-               </div>
-               <div className="iconic-products__grid-content">
-                  <Image
-                     src="/Popular2.jpeg"
-                     fluid
-                     alt="Product 2"
-                     style={{ marginTop: '1rem' }}
-                  />
-                  <div className="description">
-                     <p>Chocolate</p>
-                     <p>Semiamargo</p>
+               </Grid.Column>
+               <Grid.Column width={8}>
+                  <div className="iconic-products_container-content">
+                     <Image src="/Popular3.jpeg" fluid alt="Product 3" />
+                     <div className="iconic-products_container-content-description">
+                        <p>Chocolate</p>
+                        <p>Semiamargo</p>
+                     </div>
+                     <div className="iconic-products_container-content-btnAction-right_2">
+                        <Button type="button" primary>
+                           DESCUBRE
+                        </Button>
+                     </div>
                   </div>
-                  <div className="btnAction-left">
-                     <Button type="button" basic>
-                        MORE
-                     </Button>
-                  </div>
-               </div>
-            </Grid.Column>
-            <Grid.Column width={8}>
-               <div className="iconic-products__grid-content">
-                  <Image src="/Popular3.jpeg" fluid alt="Product 3" />
-                  <div className="description">
-                     <p>Our Iconic</p>
-                     <p>Tequila Chocolate</p>
-                  </div>
-                  <div className="btnAction-right_2">
-                     <Button type="button" primary>
-                        SHOP NOW
-                     </Button>
-                  </div>
-               </div>
-            </Grid.Column>
-         </Grid>
-      </Container>
+               </Grid.Column>
+            </Grid>
+         </Container>
+      </section>
    );
 }
