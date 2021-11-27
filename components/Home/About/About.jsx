f88b4ff2f0104e5f63cafde2615c14c942f47353
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-26 20:33:09 -0600

    Change info About

--- components/Home/About/About.jsx
+++ components/Home/About/About.jsx
@@ -1,34 +1,28 @@
-import React from 'react';
+import Link from 'next/link';
 import { Button, Container, Grid, Image } from 'semantic-ui-react';
 
 export default function About() {
    return (
-      <Container fluid className="about">
+      <section className="about">
          <Container fluid className="about-container">
             <Grid>
                <Grid.Column width={8} className="about-container-description">
                   <h2>Todo el sabor europeo desde 1987</h2>
                   <p>
-                     Occaecat velit enim veniam exercitation. Laboris nostrud
-                     duis sit mollit sint consectetur ipsum duis officia do do
-                     do ea proident. Ipsum anim id fugiat aliqua adipisicing
-                     duis. Nisi irure incididunt laboris est duis. Consectetur
-                     occaecat officia adipisicing et minim occaecat ut mollit
-                     esse. Exercitation dolor magna commodo occaecat pariatur
-                     commodo commodo reprehenderit adipisicing. Consectetur
-                     nulla eu et reprehenderit culpa dolor veniam elit
-                     consectetur aliquip culpa fugiat consectetur. Veniam
-                     consequat sint qui ea nulla ex amet dolore sunt ullamco
-                     velit sunt voluptate. Tempor aute est non eu minim esse ad
-                     qui ad.
+                     Somos una empresa con más de 30 años de experiencia en el
+                     mercado. Nos dedicamos a la producción de chocolatería y
+                     bombonería artesanal elaborada por expertos chocolateros
+                     mexicanos.
                   </p>
-                  <Button basic>DISCOVER MORE</Button>
+                  <Link href="/xocobel">
+                     <Button basic>MÁS</Button>
+                  </Link>
                </Grid.Column>
                <Grid.Column width={8}>
-                  <Image src="/chocolate-about.jpg" fluid />
+                  <Image src="/chocolate-about.jpg" />
                </Grid.Column>
             </Grid>
          </Container>
-      </Container>
+      </section>
    );
 }
