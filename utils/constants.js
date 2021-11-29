18fc1cbcaea49a2b7e01d70d5dc4b7dd9e6fab93
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-29 11:35:25 -0600

    Add font Flama

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_PATH = 'http://localhost:1337';
+export const BASE_PATH = 'https://xocobel.herokuapp.com';
+// export const BASE_PATH = 'http://localhost:1337';
 export const TOKEN = 'token';
 export const CART = 'cart';
 export const STRIPE_TOKEN =
