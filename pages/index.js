45a71d3febcfcdd9944a07db524c67cc0db6ecee
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-30 00:31:14 -0600

    Change Dynamic PopularProducts

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import Seo from '../components/Seo.jsx';
+import dynamic from 'next/dynamic';
 import MainBanner from '../components/Home/MainBanner';
 import BasicLayout from '../layouts/Basic/BasicLayout';
-import PopularProducts from '../components/Home/PopularProducts/PopularProducts.jsx';
 import About from '../components/Home/About/About.jsx';
 import Ingredients from '../components/Home/Ingredients/Ingredients.jsx';
 import Contact from '../components/Home/Contact/Contact.jsx';
 import Shipping from '../components/Home/Shipping/Shipping.jsx';
 import IconicProducts from '../components/Home/IconicProducts/IconicProducts.jsx';
 
+const DynamicPopularProducts = dynamic(() =>
+   import('../components/Home/PopularProducts/PopularProducts.jsx')
+);
+
 export default function Home() {
    return (
       <BasicLayout className="home">
          <Seo title="XOCOBEL | LO MEJOR DEL CHOCOLATE" />
          <MainBanner />
-         <PopularProducts />
+         <DynamicPopularProducts />
          <IconicProducts />
          <About />
          <Ingredients />
