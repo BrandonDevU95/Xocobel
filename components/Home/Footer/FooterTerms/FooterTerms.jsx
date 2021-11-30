cc9028314d046c2c19689c0cfad6b5fb72c85c93
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-29 21:45:32 -0600

    Add Modal Terms & Conditions

--- components/Home/Footer/FooterTerms/FooterTerms.jsx
+++ components/Home/Footer/FooterTerms/FooterTerms.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Container, Icon } from 'semantic-ui-react';
+import { useState } from 'react';
+import { Container, Icon, Button } from 'semantic-ui-react';
+import BasicModal from '../../../Modal/BasicModal';
 
 export default function FooterTerms() {
+   const [showModal, setShowModal] = useState(false);
+
    return (
       <div className="footer-terms">
          <Container fluid className="footer-terms-container">
@@ -11,11 +14,79 @@ export default function FooterTerms() {
                   <p>Xocobel SA DE CV</p>
                </div>
                <div className="footer-terms-container_content_right">
-                  {/* TODO: Hacer un modal con los términos y condiciones */}
-                  <p>Términos y Condiciones</p>
+                  <a onClick={() => setShowModal(true)}>
+                     Términos y Condiciones
+                  </a>
                </div>
             </div>
          </Container>
+         <BasicModal
+            show={showModal}
+            size="tiny"
+            setShow={setShowModal}
+            title="Términos y Condiciones"
+            className="footer-terms_modal"
+         >
+            <Terms setShowModal={setShowModal} />
+         </BasicModal>
+      </div>
+   );
+}
+
+function Terms({ setShowModal }) {
+   return (
+      <div>
+         <h1>Términos y Condiciones</h1>
+         <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+         </p>
+         <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+         </p>
+         <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text.
+         </p>
+         <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text. All the Lorem Ipsum generators on the Internet tend
+            to repeat predefined chunks as necessary, making this the first true
+            generator on the Internet. It uses a dictionary of over 200 Latin
+            words, combined with a handful of model sentence structures, to
+            generate Lorem Ipsum which looks reasonable. The generated Lorem
+            Ipsum is therefore always free from repetition, injected humour, or
+            non-characteristic words etc.
+         </p>
+         <div className="footer-terms_modal-button">
+            <Button primary onClick={() => setShowModal(false)}>
+               Aceptar
+            </Button>
+         </div>
       </div>
    );
 }
