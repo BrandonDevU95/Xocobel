28a2ab79d240b75753e910871642a4c0edd9a4a3
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-02 15:47:47 -0600

    Responsive Shipping

--- components/Home/Shipping/Shipping.jsx
+++ components/Home/Shipping/Shipping.jsx
@@ -3,38 +3,38 @@ import { Container, Grid, Image } from 'semantic-ui-react';
 
 export default function Shipping() {
    return (
-      <Container fluid className="shipping">
-         <Container fluid className="shipping-container">
-            <Grid columns="equal">
-               <Grid.Column>
-                  <div className="shipping-container_data">
-                     <div className="shipping-container_data-info">
+      <section className="shipping d-none d-md-block d-lg-block">
+         <div className="shipping-container container">
+            <div className="shipping-container_row row">
+               <div className="shipping-container_row-col col-12 col-md-4 col-lg-4">
+                  <div className="shipping-container_row-col_data">
+                     <div className="shipping-container_row-col_data-info">
                         <h3>Regalos</h3>
                         <h4>Personalizados</h4>
                      </div>
                      <Image src="/icons/gift-box.svg" />
                   </div>
-               </Grid.Column>
-               <Grid.Column>
-                  <div className="shipping-container_data">
-                     <div className="shipping-container_data-info">
+               </div>
+               <div className="shipping-container_row-col col-12 col-md-4 col-lg-4">
+                  <div className="shipping-container_row-col_data">
+                     <div className="shipping-container_row-col_data-info">
                         <h4>Envíos Fáciles y</h4>
                         <h3>Rápidos</h3>
                      </div>
                      <Image src="/icons/truck.svg" />
                   </div>
-               </Grid.Column>
-               <Grid.Column>
-                  <div className="shipping-container_data">
-                     <div className="shipping-container_data-info">
+               </div>
+               <div className="shipping-container_row-col col-12 col-md-4 col-lg-4">
+                  <div className="shipping-container_row-col_data">
+                     <div className="shipping-container_row-col_data-info">
                         <h3>Pagos</h3>
                         <h4>Seguros</h4>
                      </div>
                      <Image src="/icons/padlock.svg" />
                   </div>
-               </Grid.Column>
-            </Grid>
-         </Container>
-      </Container>
+               </div>
+            </div>
+         </div>
+      </section>
    );
 }
