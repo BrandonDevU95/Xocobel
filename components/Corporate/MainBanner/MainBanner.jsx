8c41425e7c5222a27280ca399a3eae13848b4568
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-03 17:49:58 -0600

    Responsive MainBanner Corporate

--- components/Corporate/MainBanner/MainBanner.jsx
+++ components/Corporate/MainBanner/MainBanner.jsx
@@ -1,28 +1,29 @@
-import { Container, Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 export default function MainBanner() {
    return (
-      <section className="main-banner-corporate-container">
-         <Container fluid className="main-banner-corporate-container__banner">
-            <Grid>
-               <Grid.Column width={7}>
-                  <div className="main-banner-corporate-container__banner_left">
-                     <h1>Regalos de Empresa</h1>
-                     <p>
-                        Lorem esse consequat amet ullamco sint ullamco consequat
-                        in amet dolor consectetur sit duis ex. Id incididunt
-                        mollit amet sint elit ex do consequat quis laboris ea.
-                        Culpa irure qui aliqua irure mollit amet excepteur
-                        adipisicing culpa pariatur ipsum laborum. Magna sint
-                        duis id qui Lorem elit ad laboris enim id. Aliquip nisi
-                        aliquip nisi fugiat aute est laborum irure proident
-                        fugiat.
-                     </p>
+      <section className="main-banner-corporate">
+         <div className="main-banner-corporate_banner">
+            <div className="main-banner-corporate_banner-container container">
+               <div className="main-banner-corporate_banner-container_row row">
+                  <div className="main-banner-corporate_banner-container_row-col col-12 col-lg-8 col-xl-6">
+                     <div className="main-banner-corporate_banner-container_row-col-title">
+                        <h1>Regalos de Empresa</h1>
+                        <p>
+                           Lorem esse consequat amet ullamco sint ullamco
+                           consequat in amet dolor consectetur sit duis ex. Id
+                           incididunt mollit amet sint elit ex do consequat quis
+                           laboris ea. Culpa irure qui aliqua irure mollit amet
+                           excepteur adipisicing culpa pariatur ipsum laborum.
+                           Magna sint duis id qui Lorem elit ad laboris enim id.
+                           Aliquip nisi aliquip nisi fugiat aute est laborum
+                           irure proident fugiat.
+                        </p>
+                     </div>
                   </div>
-               </Grid.Column>
-               <Grid.Column width={9}></Grid.Column>
-            </Grid>
-         </Container>
+               </div>
+            </div>
+         </div>
       </section>
    );
 }
