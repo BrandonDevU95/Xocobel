4220c3665ad0f9bad13953fa06ae2451e07f1d40
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-03 17:30:55 -0600

    Responsive Events

--- pages/events.js
+++ pages/events.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Slider from 'react-slick';
 import BasicLayout from '../layouts/Basic';
 import Contact from '../components/Home/Contact';
-import { Container, Grid, Image } from 'semantic-ui-react';
+import Image from 'next/image';
+import Seo from '../components/Seo';
+import EventsBanner from '../public/events-banner.webp';
+import Slide1 from '../public/events-slide-1.webp';
+import Slide2 from '../public/events-slide-2.webp';
+import Slide3 from '../public/events-slide-3.webp';
+import Slide4 from '../public/events-slide-4.webp';
 
 export default function Events() {
    const settings = {
@@ -16,20 +22,19 @@ export default function Events() {
 
    return (
       <BasicLayout className="events">
-         <Container fluid className="events-container">
-            <div className="events-container_info">
-               <Grid>
-                  <Grid.Column width={8}>
-                     <Image
-                        fluid
-                        src="/events-banner.webp"
-                        alt="Events Banner"
-                     />
-                  </Grid.Column>
-                  <Grid.Column width={8}>
-                     <div className="events-container_info-description">
-                        <h2>Tu Evento con Xocobel</h2>
-                        <p>
+         <Seo title="EVENTOS | LOS MEJORES EVENTOS CON XOCOBEL" />
+         <section className="pt-5">
+            <div className="events-container container">
+               <div className="events-container_row row">
+                  <div className="events-container_row-col col-12 col-lg-6 order-2 order-lg-1">
+                     <Image src={EventsBanner} alt="Events Banner" />
+                  </div>
+                  <div className="events-container_row-col col-12 col-lg-6 order-1 order-lg-2">
+                     <div className="events-container_row-col-description">
+                        <h2 className="h1 text-center">
+                           Tu Evento con Xocobel
+                        </h2>
+                        <p className="h4">
                            Amamos crear experiencias geniales y memorables,
                            mediante el diseño, personalización y montaje de
                            mesas de postres para cualquier tipo de evento u
@@ -43,41 +48,37 @@ export default function Events() {
                            especial, único e irrepetible!
                         </p>
                      </div>
-                  </Grid.Column>
-               </Grid>
+                  </div>
+               </div>
+               <SliderEvents
+                  settings={settings}
+                  Slider1={Slide1}
+                  Slider2={Slide2}
+                  Slider3={Slide3}
+                  Slider4={Slide4}
+               />
             </div>
-            <Slider {...settings}>
-               <div>
-                  <Image
-                     fluid
-                     src="/events-slide-1.webp"
-                     alt="Events Slide 1"
-                  />
-               </div>
-               <div>
-                  <Image
-                     fluid
-                     src="/events-slide-2.webp"
-                     alt="Events Slide 2"
-                  />
-               </div>
-               <div>
-                  <Image
-                     fluid
-                     src="/events-slide-3.webp"
-                     alt="Events Slide 3"
-                  />
-               </div>
-               <div>
-                  <Image
-                     fluid
-                     src="/events-slide-4.webp"
-                     alt="Events Slide 4"
-                  />
-               </div>
-            </Slider>
             <Contact />
-         </Container>
+         </section>
       </BasicLayout>
+   );
+}
+
+function SliderEvents({ settings, Slider1, Slider2, Slider3, Slider4 }) {
+   return (
+      <Slider {...settings}>
+         <div>
+            <Image src={Slider1} alt="Events Slide 1" />
+         </div>
+         <div>
+            <Image src={Slider2} alt="Events Slide 2" />
+         </div>
+         <div>
+            <Image src={Slider3} alt="Events Slide 3" />
+         </div>
+         <div>
+            <Image src={Slider4} alt="Events Slide 4" />
+         </div>
+      </Slider>
    );
 }
