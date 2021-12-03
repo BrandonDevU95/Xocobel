1c95cbb7d0ebf6dc43af97e7a8bd54e93660ae2c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-03 14:46:00 -0600

    Change render image ListProducts

--- components/ListProducts/ListProducts.jsx
+++ components/ListProducts/ListProducts.jsx
@@ -1,31 +1,38 @@
-import { map } from 'lodash';
+import { map, size } from 'lodash';
 import Link from 'next/link';
+import { useEffect } from 'react';
 import { Image } from 'semantic-ui-react';
 import { BASE_PATH } from '../../utils/constants';
 
-export default function ListProducts({ products, size = 'thumbnail' }) {
+export default function ListProducts({ products, sizeImg = 'thumbnail' }) {
    return (
       <div className="list-products">
          <div className="list-products_row row">
             {map(products, (product) => (
-               <Product key={product._id} product={product} size={size} />
+               <Product key={product._id} product={product} sizeImg={sizeImg} />
             ))}
          </div>
       </div>
    );
 }
 
-function Product({ product, size }) {
-   const sizeImg =
-      size === 'large'
-         ? BASE_PATH + product.poster?.formats?.large?.url
-         : size === 'medium'
-         ? BASE_PATH + product.poster?.formats?.medium?.url
-         : size === 'small'
-         ? BASE_PATH + product.poster?.formats?.small?.url
-         : size === 'thumbnail'
-         ? BASE_PATH + product.poster?.formats?.thumbnail?.url
-         : '';
+function Product({ product, sizeImg }) {
+   let imgUrl;
+
+   if (size(product.poster.formats) === 4) {
+      imgUrl =
+         sizeImg === 'large'
+            ? BASE_PATH + product.poster.formats.large.url
+            : sizeImg === 'medium'
+            ? BASE_PATH + product.poster.formats.medium.url
+            : sizeImg === 'small'
+            ? BASE_PATH + product.poster.formats.small.url
+            : sizeImg === 'thumbnail'
+            ? BASE_PATH + product.poster.formats.thumbnail.url
+            : '';
+   } else {
+      imgUrl = BASE_PATH + product.poster.url;
+   }
 
    return (
       <div className="list-products_row-col col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
@@ -33,7 +40,7 @@ function Product({ product, size }) {
             <Link href={`/${product.url}`}>
                <a>
                   <div className="list-products_row-col__product-poster">
-                     <Image src={sizeImg} alt={product.title} fluid />
+                     <Image src={imgUrl} alt={product.title} fluid />
                      <div className="list-products_row-col__product-poster-info">
                         {product.discount ? (
                            <span className="discount">
