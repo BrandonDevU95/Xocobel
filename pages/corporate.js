17110cb4ce58c5ac48bdfb0c08c030ad8ef37d60
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-03 18:20:04 -0600

    Fix container Corporate

--- pages/corporate.js
+++ pages/corporate.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import { Container } from 'semantic-ui-react';
 import Catalogs from '../components/Corporate/Catalogs';
 import MainBanner from '../components/Corporate/MainBanner';
 import ServiceInfo from '../components/Corporate/ServiceInfo';
 import Contact from '../components/Home/Contact';
-import Shipping from '../components/Home/Shipping';
 import BasicLayout from '../layouts/Basic';
 
 export default function Corporate() {
    return (
       <BasicLayout className="corporate">
-         <Container fluid className="corporate-container">
+         <div className="corporate-container container-fluid p-0">
             <MainBanner />
             <Catalogs />
             <ServiceInfo />
             <Contact />
-         </Container>
+         </div>
       </BasicLayout>
    );
 }
