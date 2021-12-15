0a8c0f5dbae318f6e61c39186a168824c8071a13
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-14 22:30:20 -0600

    Change key public strapi

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,8 @@
 // export const BASE_PATH = 'https://xoco-backend-4z9kv.ondigitalocean.app';
 // export const BASE_PATH = 'https://xocobel.herokuapp.com';
-export const BASE_PATH = 'http://localhost:1337';
+// export const BASE_PATH = 'http://localhost:1337';
+export const BASE_PATH = 'https://xocobel-backend-h4opq.ondigitalocean.app';
 export const TOKEN = 'token';
 export const CART = 'cart';
 export const STRIPE_TOKEN =
-   'pk_test_51JqMw4FDgkDMxEY6iVHTjvBlh9hlR0zWuSykGwZlLxHoIkZ1ljVzHe6TrAYaU4DWjqRqBD67LfFMRZlFSz6XMBgr003sRtJGjO';
+   'pk_test_51K6eSfERL1233vx4YaVmV3kyjYpn1wdyqrDE6iMII2jQ7QqOEleA0gvHe7zn4pAdMfinkpM6XN9pKkWAFBlKYfgB001RsSp5om';
