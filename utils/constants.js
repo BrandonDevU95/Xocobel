da6c0419f0554c5d0dbc8845bf3c376414ed0240
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-19 20:14:14 -0600

    Change stripe token

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,5 @@
-// export const BASE_PATH = 'https://xoco-backend-4z9kv.ondigitalocean.app';
-// export const BASE_PATH = 'https://xocobel.herokuapp.com';
-// export const BASE_PATH = 'http://localhost:1337';
 export const BASE_PATH = 'https://xocobel-backend-h4opq.ondigitalocean.app';
 export const TOKEN = 'token';
 export const CART = 'cart';
 export const STRIPE_TOKEN =
-   'pk_test_51K6eSfERL1233vx4YaVmV3kyjYpn1wdyqrDE6iMII2jQ7QqOEleA0gvHe7zn4pAdMfinkpM6XN9pKkWAFBlKYfgB001RsSp5om';
+   'pk_live_51K6eSfERL1233vx4Q73ayI6q5GEOH80QfLuGdUwUo3uaXxLR6qAi7hmDEeZzUg4rpLPzmeWSe3FCiI9pJnb8Lcs500X9dOBtBh';
